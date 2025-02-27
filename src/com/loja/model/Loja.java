1a756089d77a4422c70afe7979587301000b2211
Michellmfp <michellmfp@gmail.com>
2025-02-27 11:04:02 -0300

    Adicionar classe Loja com métodos para gerenciar produtos e pedidos

--- src/com/loja/model/Loja.java
+++ src/com/loja/model/Loja.java
@@ -13,4 +13,29 @@ public class Loja {
         this.produtos = new ArrayList<>();
         this.pedidos = new ArrayList<>();
     }
+
+    public void adicionarProduto(Produto produto) {
+        this.produtos.add(produto);
+    }
+
+    public List<Produto> listarProdutos() {
+        return produtos;
+    }
+
+    public void adicionarPedido(Pedido pedido) {
+        this.pedidos.add(pedido);
+    }
+
+    public List<Pedido> listarPedidos() {
+        return pedidos;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Loja: " + nome + "\nProdutos: " + produtos + "\nPedidos: " + pedidos;
+    }
 }
